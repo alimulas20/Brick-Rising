f3e85c8388ca658b9378cffbb83e5216ad7d795c
alimulas20 <98461025+alimulas20@users.noreply.github.com>
2022-08-09 17:18:00 +0300

    Starting

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -8,15 +8,13 @@ public class Timer : MonoBehaviour
     public Slider timer;
     public float gameTime;
     bool stopTimer;
-    // Start is called before the first frame update
+   
     void Start()
     {
         stopTimer = false;
         timer.maxValue = gameTime;
         timer.value = gameTime;
     }
-
-    // Update is called once per frame
     void Update()
     {
         float time = gameTime - Time.time;
